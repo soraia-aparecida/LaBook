ecafe9dc8a2420592b83d83f762d703dbe7426c0
soraia-aparecida <soraia211norte@gmail.com>
2022-03-15 15:37:13 -0300

    criei o app.ts

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,9 @@
-import express from 'express'
-import cors from 'cors'
+import app from './app'
 import { userRouter } from './routes/userRouter'
 import { postRouter } from './routes/postRouter'
 import { feedRouter } from './routes/feedRouter'
-import dotenv from 'dotenv'
-
-dotenv.config()
-const app = express()
-
-app.use(express.json())
-app.use(cors())
 
 app.use('/user', userRouter)
 app.use('/post', postRouter)
 app.use('/feed', feedRouter)
 
-app.listen(process.env.PORT || 3003, () => {
-    console.log('Servidor rodando na porta 3003')
-})
